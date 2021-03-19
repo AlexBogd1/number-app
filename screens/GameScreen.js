44d971f45509e6923d77e993df751c47b91aa403
AlexB <7894664@mail.ru>
2021-03-19 13:41:08 +0300

    Add switching between screens

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
 import Color from "../constants/colors";
 
 const generateRandomBetween = (min, max, exclude) => {
@@ -19,10 +20,10 @@ const GameScreen = (props) => {
     generateRandomBetween(1, 100, props.userChoice)
   );
   return (
-    <View>
+    <View style={styles.screen}>
       <Text>Opponent's Guess</Text>
       <NumberContainer>{currentGuess}</NumberContainer>
-      <Card>
+      <Card style={styles.buttonContainer}>
         <Button title="LOWER" onPress={() => {}} />
         <Button title="GREATER" onPress={() => {}} />
       </Card>
@@ -30,6 +31,19 @@ const GameScreen = (props) => {
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginTop: 20,
+        width: 300,
+        maxWidth: '80%',
+    }
+});
 
 export default GameScreen;
